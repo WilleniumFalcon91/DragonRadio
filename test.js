60e6add762fcea30a7dd896225b04cf78a3c82fa
John Hartert <j.blainehartert@gmail.com>
2017-08-16 16:47:29 -0400

    Ajax call

---
+++ test.js
@@ -0,0 +1,6 @@
+// const getTopTracks = $.get('https://api.napster.com/v2.1/tracks/top?apikey=NmYxOWEyYmUtZDc0MC00NWIyLWIxYWEtNjg4YmE5YmU2YTg4');
+
+// getTopTracks
+//   .then((response) => {
+//     $tracks.html(tracksTemplate(response));
+//   });
