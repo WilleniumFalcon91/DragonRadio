2835bcb4cbd79bd0ae96ee5bab09485150115ea9
John Hartert <j.blainehartert@gmail.com>
2017-08-16 14:23:11 -0400

    Here's server stuff

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 document.write(process.version);
+var net = require('net');
+var client = net.connect({port: 8080}, function() {
+   console.log('Connection established!');  
+    });
 
-module.exports = app;
+client.on('data', function(data) {
+   document.write(data.toString());
+   client.end();
+    });
+
+client.on('end', function() { 
+   console.log('Disconnected :(');
+    });
+
+
+module.exports = require;
