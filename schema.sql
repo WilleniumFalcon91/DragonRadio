f8310ad816e85a18cd626eef2dc780404a4dbba9
John Hartert <j.blainehartert@gmail.com>
2017-08-22 11:32:28 -0400

    Adds new table info

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-create table users (
-    user_id serial primary key, 
-    name varchar (100) not null, 
-    email varchar (200)  not null
+create table activity (
+    playlist_clicks serial primary key, 
+    -- name varchar (100) not null, 
+    -- email varchar (200)  not null
 );
